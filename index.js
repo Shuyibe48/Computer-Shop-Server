02b93f76b47abdab7af0ccaf7711beec7f703467
Shuyibe48 <gthrough48@gmail.com>
2023-07-16 20:22:42 +0600

    connect with mongodb

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 5000
+
+
+// middle were
+const corsOptions = {
+    origin: '*',
+    credentials: true,
+    optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions))
+app.use(express.json())
+
+
+
+
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.jukjd3u.mongodb.net/?retryWrites=true&w=majority`
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+
+
+
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+    }
+}
+run().catch(console.dir);
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Computer Shop server is running..')
+})
+
+app.listen(port, () => {
+    console.log(`Computer Shop server is running on port ${port}`)
+})
